1ad97bca4b06ccf4535d0721ca394976d5e1d5ac
JonathanSkills <jonathanromero@upeu.edu.pe>
2020-09-23 04:46:18 +0000

    Parte 5 terminada

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,11 +1,28 @@
 from flask.cli import FlaskGroup
+import sys
+import unittest
+from project import create_app, db   # nuevo
+from project.api.models import User  # nuevo
 
-from project import app
+#cli = FlaskGroup(app)
+app = create_app()  # new
+cli = FlaskGroup(create_app=create_app)  # nuevo
 
+# new
+@cli.command('recreate_db')
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
-cli = FlaskGroup(app)
-
+@cli.command()
+def test():
+    """Ejecuta las pruebas sin cobertura de código"""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    sys.exit(result)
 
 if __name__ == '__main__':
     cli()
-
